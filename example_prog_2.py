106c7fa94ef796506bc1e61224f1eaf332896d7c
a head full of dreams <a head full of dreams>
2021-12-07 19:57:00 +0530

    second example of type casting

--- example_prog_2.py
+++ example_prog_2.py
@@ -1,6 +1,7 @@
-int_number = input("Enter the Integer Number: ")
-deci_number = input("Enter the Decimal Number: ")
-
-result = float(deci_number) + float(int_number)
-
+#Type casting during runtime
+int_number = float(input("Enter the Integer Number: "))
+deci_number = float(input("Enter the Decimal Number: "))
+#no need for Type Casting
+result = deci_number + int_number
+#displaying Result
 print("The result of the addition is: ", result)
