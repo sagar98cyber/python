a0aa5ac771325c63704a3892172e715a2e4af7cf
a head full of dreams <a head full of dreams>
2021-12-07 19:48:32 +0530

    user input data type conversion example done

---
+++ input_type_conversion.py
@@ -0,0 +1,12 @@
+birth_year = input("Enter your birth year: ")
+#Converting Input to Integer so that the operands can be of similar datatype to be processed
+age = 2020 - int(birth_year)
+
+print(age)
+
+# Datatype conversion Functions 
+# int()
+# float()
+# bool()
+# str()
+
--- third_python_program.py
+++
@@ -1,5 +0,0 @@
-#TAKING INPUT FROM THE USER
-
-user_input = input("Taking input from the user: ")
-
-print("Input is: ",user_input)
