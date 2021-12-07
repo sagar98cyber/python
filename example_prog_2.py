ac6f48412607df5672c285d70dabec990d5db66a
a head full of dreams <a head full of dreams>
2021-12-07 19:52:58 +0530

    second example of type casting

---
+++ example_prog_2.py
@@ -0,0 +1,6 @@
+int_number = input("Enter the Integer Number: ")
+deci_number = input("Enter the Decimal Number: ")
+
+result = float(deci_number) + float(int_number)
+
+print("The result of the addition is: ", result)
