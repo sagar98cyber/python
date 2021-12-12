ac0f09df4d59d1ec3dbbeaf97a9c01b7a493c1e4
a head full of dreams <a head full of dreams>
2021-12-12 13:25:04 +0530

    Formatted String

---
+++ formatted_string.py
@@ -0,0 +1,12 @@
+first = 'John'
+last = 'Smith'
+#Suppose you wanna print 
+# John [Smith] is a coder!
+
+#You have to do all ..... 
+message = first + ' [' + last + '] is a coder!'
+
+#Honestly this is a really difficult thing to visualize for a coder so we use formatted String
+msg = f'{first} [{last}] is a coder!'
+
+# f' ..... ' Is how we represent Formatted String
