30ccc7fd14fce99940b6b3d1c5ec39723261622c
a head full of dreams <a head full of dreams>
2021-12-24 10:51:22 +0530

    Optimizing the Program

--- zero_tenth_weight_converter.py
+++ zero_tenth_weight_converter.py
@@ -1,11 +1,18 @@
 weight = int(input('Enter your Weight: '))
 p_or_k = str(input('Entered in (L)bs or (K)ilos: '))
 dis_weight = 0
-if p_or_k == 'k' or p_or_k == 'K':
-    dis_weight = weight*2
+#this is what I did
+#if p_or_k == 'k' or p_or_k == 'K':
+
+#optimizing the code:
+if p_or_k.upper() == 'K':
+    dis_weight = weight/0.45
     print(f'The entered weight is in Kilos: {weight} \n The corresponding Lbs for that is: {dis_weight}')
-elif p_or_k == 'l' or p_or_k == 'L':
-    dis_weight = weight/2
+#similarly I did this to improve the code below
+#elif p_or_k == 'l' or p_or_k == 'L':
+#we do this:
+elif p_or_k.upper() == 'L':
+    dis_weight = weight*0.45
     print(f'The entered weight is in Lbs: {weight} \n The corresponding Kilos for that is: {dis_weight}')
 else:
     print("Enter a valid Input dude!")
