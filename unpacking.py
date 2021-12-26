9f5e428083e2de8d3ed33bf0fa5aff1762f87e7c
a head full of dreams <a head full of dreams>
2021-12-26 11:32:49 +0530

    Dictionaries Done

--- unpacking.py
+++
@@ -1,8 +0,0 @@
-coordinates = (1,2,3)
-
-#instead  of storing a single value of an index of a tuple using unpacking to store all the values in the respective variables
-x,y,z = coordinates
-print(f'{x}, {y}, {z}')
-
-#this concept of unpacking works on LISTS as well
-#coordinates = [1,2,3]
