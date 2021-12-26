44e751767cd454a5286a375fd5148c65663393ef
a head full of dreams <a head full of dreams>
2021-12-27 01:09:51 +0530

    Random value generator

---
+++ 1_random_values.py
@@ -0,0 +1,10 @@
+import random
+
+#for i in range(5):
+#    print(random.random())
+
+#for i in range(5):
+#    print(random.randint(10,20))
+
+members = ['John','Mary','Mosh','Bob']
+print(random.choice(members))
