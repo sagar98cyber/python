59dd6c413fee69f15cce43f00d9052d373b9aaeb
Sagar Shah <57261622+sagar98cyber@users.noreply.github.com>
2021-12-30 20:35:05 +0530

    Update 2_accessing_multi_dimensional.py

--- 2_accessing_multi_dimensional.py
+++ 2_accessing_multi_dimensional.py
@@ -1,5 +1,8 @@
 # Creating a Multi-Dimensional List
 # (By Nesting a list inside a List)
-List = [['Geeks', 'For'] , ['Geeks']]
+List = [
+['Geeks', 'For'] , 
+['Geeks']
+]
 print("\nMulti-Dimensional List: ")
 print(List)
