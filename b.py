35e5473de1333cfae89abfc3ddb5357053da2dc7
a head full of dreams <a head full of dreams>
2022-01-16 01:17:03 +0530

    practical one

---
+++ b.py
@@ -0,0 +1,13 @@
+import a as a
+
+print(type(a.x))
+
+print(type(a.y))
+
+print(type(a.z))
+
+print(type(a.Dict))
+
+print(type(a.thistuple))
+
+print(type(a.numbers))
---
+++ c.py
@@ -0,0 +1,9 @@
+print('''
+1. mathematical operation 
+2. area of triangle, square, rectangle
+3. surface area of cone and cube
+4. volume of cube, cone, sphere
+''')
+
+user_input = input(">: ")
+
