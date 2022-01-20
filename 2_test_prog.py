0edff7cdc95775b4a8579d9175a17ec75e984d5d
a head full of dreams <a head full of dreams>
2022-01-20 11:30:11 +0530

    ramya sir pracs conflict resolved

--- 2_test_prog.py
+++
@@ -1,8 +0,0 @@
-has_good_credit = False
-price_house = 1000000
-if has_good_credit:
-    put_down = 0.1 * price_house
-else:
-    put_down = 0.2 * price_house
-
-print(f'Down payment of the hpuse: {put_down}')
