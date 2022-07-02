262c1ae26a0d5ef7830ca7b7834c9fe15ea8b7b8
a head full of dreams <a head full of dreams>
2022-07-02 19:39:52 +0530

    Storing a public, Private key

---
+++ file transfer/client/cipher.py
@@ -0,0 +1,91 @@
+import random
+
+def isPrime(x):
+    i = 2
+    while i*i <= x:          # Only checks for factors up to the square root of x
+        if x%i==0:
+            return False
+        i+=1
+    return True
+
+# Generates an n-digit prime number
+def primeGenerator(n):
+    string = ""
+    for i in range(n):
+        x=random.randint(1,9)
+        string += str(x)
+    number = int(string)
+
+    if number%2==0:         # Eliminating even numbers
+        number+=1
+
+    while True:
+        if isPrime(number):
+            return number
+        number+=2
+
+# Finds the multiplicative inverse of a mod b
+def pulverizer(a,b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x, y = pulverizer(b % a, a)
+        d = y - (b // a) * x
+        return gcd, d, x
+
+# Generates a secret key and a public key
+def keyGen():
+    numberOfDigits = 10
+    p = primeGenerator(numberOfDigits)        # First random prime number
+    q = primeGenerator(numberOfDigits)        # Second random prime number
+
+    n = p*q
+    r = (p-1)*(q-1)                           # Euler's Totient
+    e = primeGenerator(numberOfDigits)**2     # A number that is highly likely to be relatively prime with r
+
+    publicKey = (e,n)
+
+    g, d, y = pulverizer(e,r)
+    if d<0:
+        d %= r
+
+    secretKey = (d,n)
+
+    return secretKey, publicKey
+
+def encrypt(message, publicKey):
+    (e,n) = publicKey
+    encryptedMessage = []
+    for letter in message:
+        letter = ord(letter)
+        encryptedLetter = pow(letter, e, n)
+        encryptedMessage.append(encryptedLetter)
+
+    return encryptedMessage
+
+def decrypt(encryptedMessage, secretKey):
+    (d,n) = secretKey
+    originalMessage = ""
+
+    for number in encryptedMessage:
+        originalNumber = pow(number, d, n)
+        originalMessage += chr(originalNumber)
+
+    return originalMessage
+
+#def main():
+#    message = bytes(input("Input the message to encrypt: "),"utf-8")
+#    print(type(message))
+#    message = message.decode()
+#    print(type(message))
+#    secretKey, publicKey = keyGen()
+
+#    encryptedMessage = encrypt(message,publicKey)
+
+#    print("\nEncrypted text:", encryptedMessage)
+#    print("\nThe secret key is:", secretKey)
+
+ #   originalMessage = decrypt(encryptedMessage,secretKey)
+ #   print("\nDecrypted Text:", originalMessage)
+
+#main()
--- file transfer/client/client.py
+++ file transfer/client/client.py
@@ -4,13 +4,49 @@ Client that sends the file (uploads)
 import socket
 import tqdm
 import os
-import argparse
+import cipher as c
 
 SEPARATOR = "<SEPARATOR>"
 
-BUFFER_SIZE = 1024 * 4
+BUFFER_SIZE = 256 * 4
+PUBLIC_KEY =""
+PRI_KEY=""
+
+def writeOnFilePublic(data):
+    file_to_write_on = open("Public.txt", 'w')
+    #file_to_write_on.writelines(bytes(data,"utf-8"))
+    file_to_write_on.writelines(data)
+    return None
+
+def writeOnFilePrivate(data):
+    file_to_write_on = open("Private.txt", 'w')
+    #file_to_write_on.writelines(bytes(data,"utf-8"))
+    file_to_write_on.writelines(data)
+    return None
+
+def readPublicFile():
+        with open("Public.txt", "rb") as f:
+                while True:
+                    # read the bytes from the file
+                    bytes_read = f.read(BUFFER_SIZE)
+                    PUBLIC_KEY = PUBLIC_KEY+ bytes_read
+                    if not bytes_read:
+                        # file transmitting is done
+                        break
+
+def readPrivateFile():
+        with open("Private.txt", "rb") as f:
+                while True:
+                    # read the bytes from the file
+                    bytes_read = f.read(BUFFER_SIZE)
+                    PRI_KEY = PRI_KEY + bytes_read
+                    if not bytes_read:
+                        # file transmitting is done
+                        break
+
 
 #def encrypt_file_rsa_algo_public_key(bytes):
+
 
 def send_file(filename, host, port):
     # get the file size
@@ -20,6 +56,7 @@ def send_file(filename, host, port):
     print(f"[+] Connecting to {host}:{port}")
     s.connect((host, port))
     print("[+] Connected.")
+
     #filename = encrypt_file_rsa_algo_public_key(filename)
     # send the filename and filesize
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
@@ -30,13 +67,14 @@ def send_file(filename, host, port):
         while True:
             # read the bytes from the file
             bytes_read = f.read(BUFFER_SIZE)
+            #print(f"\n\n {type(bytes_read)}")
             if not bytes_read:
                 # file transmitting is done
                 break
-            # we use sendall to assure transimission in 
-            # busy networks
             #before  transmission read all the bytes encrypt it
             #bytes_read = encrypt_file_rsa_algo_public_key(bytes_read)
+            # we use sendall to assure transimission in 
+            # busy networks
             s.sendall(bytes_read)
             # update the progress bar
             progress.update(len(bytes_read))
@@ -44,7 +82,15 @@ def send_file(filename, host, port):
     # close the socket
     s.close()
 
-fileName = input("Enter the filename you want to send.\n If it is in the same directory then share the \"relative path\" or if it is not in the current working directory share the \"absolute path\" of the file:\n")
-host = input("Enter the HOST ADDRESS you want to connect to:")
+#fileName = input("Enter the filename you want to send.\n If it is in the same directory then share the \"relative path\" or if it is not in the current working directory share the \"absolute path\" of the file:\n")
+#host = input("Enter the HOST ADDRESS you want to connect to:")
+fileName = "data.csv"
+host="127.0.0.1"
 port = 5001
+secretKey,publicKey = c.keyGen()
+print(f"\n\n {secretKey}")
+print(f"\n\n {publicKey}")
+writeOnFilePublic(str(publicKey))
+writeOnFilePrivate(str(secretKey))
+
 send_file(filename=fileName,host=host,port=port)
