436b85c7570612ad08cba98f821cc3e0ad9327af
a head full of dreams <a head full of dreams>
2022-07-03 15:06:46 +0530

    file copy function created

--- file transfer/client/readWrite.py
+++ file transfer/client/readWrite.py
@@ -1,4 +1,10 @@
+import os
 fileName = "data.csv"
+if os.path.isdir("output"): 
+    print('exists')
+else: 
+    os.mkdir("output")
 
-with open(fileName, "rb") as in_file, open("out.csv", "wb") as out_file:
+#with open(fileName, "rb") as in_file, open("out.csv", "wb") as out_file:
+with open(fileName, "rb") as in_file, open(f'output\{fileName}', "wb") as out_file:
     out_file.write(in_file.read())
